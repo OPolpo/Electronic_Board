6e4b3a0f05ed6eaa7de86066f40ab64bcd96f949
OPolpo <boccheseandrea@gmail.com>
2014-04-01 23:00:45 +0200

    first experiment with db connection ad queering

--- Board.php
+++ Board.php
@@ -2,12 +2,23 @@
 
 class Board{
 	private static $hostName = "localhost";
-	private static $userName = "root";
-	private static $password = "";
+	private static $userName = "webuser";
+	private static $password = "dummypass";
 	private static $dbName = "tia";
 
 	public static function getHTML(){
+		$db = new mysqli("127.0.0.1", self::$userName, self::$password, self::$dbName, 8889);
+    	
+    	if ($db->connect_errno)
+    	    echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
+		
+		$sql = "SELECT * FROM board";
 
+		if(!$result = $db->query($sql))
+    		die('There was an error running the query [' . $db->error . ']');
+		
+		while($row = $result->fetch_assoc())
+   			echo $row['user'] . '<br />';
 	}
 
 	public static function submit($user,$proposal){
