6476f2f7a27cf09a726ba0dab91a29d9b3ccc439
OPolpo <boccheseandrea@gmail.com>
2014-04-02 13:52:05 +0200

    status query and other stuff completed

--- Board.php
+++ Board.php
@@ -6,27 +6,42 @@ class Board{
 	private static $password = "dummypass";
 	private static $dbName = "tia";
 
-	public static function getHTML(){
-		$db = new mysqli("127.0.0.1", self::$userName, self::$password, self::$dbName, 8889);
-    	
+	public static function perform_query($query){
+		$db = new mysqli("127.0.0.1", self::$userName, self::$password, self::$dbName, 8889);//connecting to dbq
     	if ($db->connect_errno)
     	    echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
 		
-		$sql = "SELECT * FROM board";
-
-		if(!$result = $db->query($sql))
+		if(!$result = $db->query($query))
     		die('There was an error running the query [' . $db->error . ']');
+
+    	$db->close();//Closing Connection
+
+		return $result;
+	}
+
+	public static function getHTML(){
+		$result=self::perform_query("SELECT * FROM board WHERE proposal IS NOT NULL");
 		
+   		printf("(%s)<br />",self::checkStatus());//printing status
+
+   		//printing proposals
+		print("<ul>");
 		while($row = $result->fetch_assoc())
-   			echo $row['user'] . '<br />';
+   			printf("<li>%s<br />",$row['proposal']);
+		print("</ul>");
 	}
 
 	public static function submit($user,$proposal){
-
+		$result=self::perform_query("INSERT INTO board VALUES(\"$user\",\"$proposal\")");//query
+		if(!$result = $db->query($sql))
+    		die('There was an error running the query [' . $db->error . ']');
 	}
 
 	public static function checkStatus(){
-
+		$result=self::perform_query("SELECT * FROM (SELECT proposal AS prop, COUNT(proposal) AS count FROM board GROUP BY proposal )AS poo HAVING count> (SELECT COUNT(*)/2 FROM board WHERE user IS NOT NULL) AND count>=all(SELECT count(proposal) FROM board GROUP BY proposal)");
+		if($row = $result->fetch_assoc())
+			return "YES =====> ".$row['prop'];
+		return "NO";
 	}
 
 	public static function sendMail($proposal){
